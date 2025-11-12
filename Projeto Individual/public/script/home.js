297d22b4fbfdd82d43dac9b03f9d8ac0d5deaa49
KKadooca <kauan.bastos@sptech.school>
2025-11-12 13:57:44 -0300

    Adicionados arquivos da API web-data-viz

---
+++ Projeto Individual/public/script/home.js
@@ -0,0 +1,34 @@
+/* ===== DISPLAY CAROUSEL ===== */
+    var saga1 = document.querySelector('.saga-1');
+    var saga2 = document.querySelector('.saga-2');
+    var saga3 = document.querySelector('.saga-3');
+
+    function forward1() {
+      saga1.style.display = 'none';
+      saga2.style.display = 'flex';
+    }
+
+    function forward2() {
+      saga2.style.display = 'none';
+      saga3.style.display = 'flex';
+    }
+
+    function forward3() {
+      saga3.style.display = 'none';
+      saga4.style.display = 'flex';
+    }
+
+    function return1() {
+      saga1.style.display = 'none';
+      saga0.style.display = 'flex';
+    }
+
+    function return2() {
+      saga2.style.display = 'none';
+      saga1.style.display = 'flex';
+    }
+
+    function return3() {
+      saga3.style.display = 'none';
+      saga2.style.display = 'flex';
+    }
