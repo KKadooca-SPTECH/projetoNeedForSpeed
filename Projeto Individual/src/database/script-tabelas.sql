8f0ccc287fa5a794fdd0ff9167f2ce19477b1f6b
KKadooca <kauan.bastos@sptech.school>
2025-11-17 14:47:17 -0300

    Script da Database movido para ../Projeto Individual/src/database

--- Projeto Individual/src/database/script-tabelas.sql
+++ Projeto Individual/src/database/script-tabelas.sql
@@ -1,62 +1,97 @@
--- Arquivo de apoio, caso você queira criar tabelas como as aqui criadas para a API funcionar.
--- Você precisa executar os comandos no banco de dados para criar as tabelas,
--- ter este arquivo aqui não significa que a tabela em seu BD estará como abaixo!
-
-/*
-comandos para mysql server
-*/
-
-CREATE DATABASE aquatech;
-
-USE aquatech;
-
-CREATE TABLE empresa (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	razao_social VARCHAR(50),
-	cnpj CHAR(14),
-	codigo_ativacao VARCHAR(50)
-);
+CREATE DATABASE NFS;
+USE NFS;
 
 CREATE TABLE usuario (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	nome VARCHAR(50),
-	email VARCHAR(50),
-	senha VARCHAR(50),
-	fk_empresa INT,
-	FOREIGN KEY (fk_empresa) REFERENCES empresa(id)
+	idUser INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(15) NOT NULL UNIQUE,
+    email VARCHAR(40) NOT NULL UNIQUE,
+    senha VARCHAR(40) NOT NULL UNIQUE
 );
 
-CREATE TABLE aviso (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	titulo VARCHAR(100),
-	descricao VARCHAR(150),
-	fk_usuario INT,
-	FOREIGN KEY (fk_usuario) REFERENCES usuario(id)
+CREATE TABLE chamado (
+	idChamado INT PRIMARY KEY AUTO_INCREMENT,
+    titulo VARCHAR(50),
+    descricao VARCHAR(255),
+    statusChamado TINYINT,
+		CONSTRAINT chk_statusChamado
+			CHECK (statusChamado IN(0,1)), -- Não respondido: 0 // Respondido: 1
+	dataInicio TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    dataFim DATETIME,
+    fkUsuario INT,
+		FOREIGN KEY (fkUsuario)
+			REFERENCES usuario(idUser)
 );
 
-create table aquario (
-/* em nossa regra de negócio, um aquario tem apenas um sensor */
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	descricao VARCHAR(300),
-	fk_empresa INT,
-	FOREIGN KEY (fk_empresa) REFERENCES empresa(id)
+CREATE TABLE quiz (
+	idQuiz INT PRIMARY KEY AUTO_INCREMENT,
+    titulo VARCHAR(45),
+    acertos INT
 );
 
-/* esta tabela deve estar de acordo com o que está em INSERT de sua API do arduino - dat-acqu-ino */
-
-create table medida (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	dht11_umidade DECIMAL,
-	dht11_temperatura DECIMAL,
-	luminosidade DECIMAL,
-	lm35_temperatura DECIMAL,
-	chave TINYINT,
-	momento DATETIME,
-	fk_aquario INT,
-	FOREIGN KEY (fk_aquario) REFERENCES aquario(id)
+CREATE TABLE tentativasQuiz (
+	idUser INT,
+    idQuiz INT,
+    tentativas INT,
+    vitorias INT,
+    desistencias INT,
+    PRIMARY KEY (idUser, idQuiz),
+		FOREIGN KEY (idUser)
+			REFERENCES usuario(idUser),
+		FOREIGN KEY (idQuiz)
+			REFERENCES quiz(idQuiz)
 );
 
-insert into empresa (razao_social, codigo_ativacao) values ('Empresa 1', 'ED145B');
-insert into empresa (razao_social, codigo_ativacao) values ('Empresa 2', 'A1B2C3');
-insert into aquario (descricao, fk_empresa) values ('Aquário de Estrela-do-mar', 1);
-insert into aquario (descricao, fk_empresa) values ('Aquário de Peixe-dourado', 2);
+CREATE TABLE questao (
+	idQuestao INT PRIMARY KEY AUTO_INCREMENT,
+    titulo VARCHAR(45),
+    pontuacao INT,
+    idQuiz INT,
+		FOREIGN KEY (idQuiz)
+			REFERENCES quiz(idQuiz)
+);
+
+CREATE TABLE alternativas (
+	idAlternativa INT AUTO_INCREMENT,
+    idQuestao INT,
+    idQuiz INT,
+		PRIMARY KEY (idAlternativa,idQuestao,idQuiz),
+        FOREIGN KEY (idQuestao)
+			REFERENCES questao(idQuestao),
+		FOREIGN KEY (idQuiz)
+			REFERENCES quiz(idQuiz),
+    titulo VARCHAR(45),
+    status_alternativas TINYINT,
+		CONSTRAINT chk_statusAlternativas
+			CHECK(status_alternativas IN(0,1)) -- Incorreta: 0 // Correta: 1
+);
+
+CREATE TABLE jogoMemoria (
+	idJogoMemoria INT PRIMARY KEY AUTO_INCREMENT,
+    dificuldade TINYINT,
+		CONSTRAINT chk_dificuldade
+			CHECK (dificuldade BETWEEN 0 AND 10) -- O tabuleiro muda de dificuldade, de acordo com sua geração pseudoaleatória
+);
+
+CREATE TABLE tentativaJogoMemoria (
+	idUser INT,
+    idJogoMemoria INT,
+		PRIMARY KEY (idUser,idJogoMemoria),
+	tentativas INT,
+    vitorias INT,
+    desistencias INT
+);
+
+CREATE TABLE imagem (
+	idImagem INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(50),
+    url VARCHAR(99),
+    statusImagem TINYINT,
+		CONSTRAINT chk_statusImagem
+			CHECK (statusImagem IN(0,1)), -- Incorreta: 0 // Correta: 1
+	fkImagemIgual INT,
+		FOREIGN KEY (fkImagemIgual)
+			REFERENCES imagem(idImagem),
+	fkJogoMemoria INT,
+		FOREIGN KEY (fkJogoMemoria)
+			REFERENCES jogoMemoria(idJogoMemoria)
+);
