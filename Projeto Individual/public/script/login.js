5eb249e445744355a7b00fcc9815218d9126211e
KKadooca <kauan.bastos@sptech.school>
2025-11-17 22:33:48 -0300

    Login e cadastro funcionando com o banco de dados!

--- Projeto Individual/public/script/login.js
+++ Projeto Individual/public/script/login.js
@@ -1,3 +1,56 @@
 function register() {
     window.location.href = 'register.html'
 }
+
+function login() {
+
+        var emailVar = input_email.value;
+        var senhaVar = input_password.value;
+
+        if (emailVar == "" || senhaVar == "") {
+            alert("Erro: preencha todos os campos corretamente!");
+            return false;
+        }
+        else {
+            setInterval(5000)
+        }
+
+        console.log("FORM LOGIN: ", emailVar);
+        console.log("FORM SENHA: ", senhaVar);
+
+        fetch("/usuarios/autenticar", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                emailServer: emailVar,
+                senhaServer: senhaVar
+            })
+        }).then(function (resposta) {
+            console.log("ESTOU NO THEN DO entrar()!")
+
+            if (resposta.ok) {
+                console.log(resposta);
+
+                resposta.json().then(json => {
+                    console.log(JSON.stringify(json));
+                    sessionStorage.EMAIL_USUARIO = json[0].email;
+                    sessionStorage.NOME_USUARIO = json[0].nome;
+                });
+
+            } else {
+
+                console.log("Houve um erro ao tentar realizar o login!");
+
+                resposta.text().then(texto => {
+                    console.error(texto);
+                });
+            }
+
+        }).catch(function (erro) {
+            console.log(erro);
+        })
+
+        return false;
+    }
--- Projeto Individual/public/script/register.js
+++ Projeto Individual/public/script/register.js
@@ -1,3 +1,78 @@
 function login() {
     window.location.href = 'login.html'
 }
+
+  function register() {
+    
+    var nomeVar = input_name.value;
+    var emailVar = input_email.value;
+    var senhaVar = input_password.value;
+    var confirmacaoSenhaVar = input_confirmation.value;
+
+    var nomeTrim = nomeVar.trim();
+    var senhaMaiuscula = senhaVar.toUpperCase();
+    var senhaMinuscula = senhaVar.toLowerCase();
+
+    // Verificando se há algum campo em branco
+    if (
+      nomeVar == "" ||
+      emailVar == "" ||
+      senhaVar == "" ||
+      confirmacaoSenhaVar == ""
+    ) {
+
+      alert("Erro: todos os campos devem estar preenchidos!");
+      return false;
+
+    } else if ( nomeTrim != nomeVar ) {
+        alert("Erro: o nickname inserido é inválido!")
+    } else if ( !emailVar.includes('@') ) {
+        alert("Erro: o email deve conter '@'");
+    } else if ( senhaVar != confirmacaoSenhaVar ) {
+        alert("Erro: As senhas não confirmam!");
+    } else if ( 
+              (senhaVar == senhaMaiuscula) || 
+              (senhaVar == senhaMinuscula) ||
+              (!senhaVar.includes(0 || 1 || 2 || 3 || 4 || 5 || 6 || 7 || 8 || 9)) ||
+              (!senhaVar.includes('!' || '@' || '#' || '$' || '%' || '&'))
+            ) {
+        alert(`
+              Erro: A senha deve conter ao menos: \n
+              -> Letras maiúsculas e minúsculas;
+              -> Um caractere numerico;
+              -> Um caractere especial: (!, @, #, $, %, &).
+              `);
+    } else {
+
+    fetch("/usuarios/cadastrar", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        // crie um atributo que recebe o valor recuperado aqui
+        // Agora vá para o arquivo routes/usuario.js
+        nomeServer: nomeVar,
+        emailServer: emailVar,
+        senhaServer: senhaVar,
+      }),
+    })
+      .then(function (resposta) {
+        console.log("resposta: ", resposta);
+
+        if (resposta.ok) {
+
+          alert("Cadastro realizado com sucesso! Redirecionando para tela de Login...");
+
+          setTimeout(() => {
+            window.location = "login.html";
+          }, "2000");
+          
+        } else {
+          alert("Houve um erro ao tentar realizar o cadastro!");
+        }
+      })
+
+    return false;
+   }
+  }
